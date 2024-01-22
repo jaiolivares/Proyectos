c1d61fc6a098ab49015efe970a800b4ffda56d45
Javier Olivares Zavala <javier.olivares.zavala@gmail.com>
2024-01-22 18:19:23 -0300

    add query

--- GastosJO/BaseDeDatos/000_Insert_Inicial.sql
+++ GastosJO/BaseDeDatos/000_Insert_Inicial.sql
@@ -3,6 +3,7 @@ GO
 DELETE CuentasBancaria
 DELETE Bancos
 DELETE TiposDeCuenta
+DELETE TiposDeTransaccion
 GO
 
 INSERT INTO Bancos VALUES ('00100','inactivo',0)
@@ -10,13 +11,17 @@ INSERT INTO Bancos VALUES ('Scotiabank','Banco Scotiabank',1)
 INSERT INTO Bancos VALUES ('Chile','Banco Chile',1)
 INSERT INTO Bancos VALUES ('Estado','Banco Estado',1)
 GO
-INSERT INTO TiposDeCuenta Values ('00100','inactivo',0)
-INSERT INTO TiposDeCuenta Values ('Corriente','Cuenta Corriente',1)
-INSERT INTO TiposDeCuenta Values ('Ahorro','Cuenta de Ahorro',1)
-INSERT INTO TiposDeCuenta Values ('Rut','Cuenta Rut',1)
-INSERT INTO TiposDeCuenta Values ('Vista','Cuenta Vista',1)
+INSERT INTO TiposDeCuenta VALUES ('00100','inactivo',0)
+INSERT INTO TiposDeCuenta VALUES ('Corriente','Cuenta Corriente',1)
+INSERT INTO TiposDeCuenta VALUES ('Ahorro','Cuenta de Ahorro',1)
+INSERT INTO TiposDeCuenta VALUES ('Rut','Cuenta Rut',1)
+INSERT INTO TiposDeCuenta VALUES ('Vista','Cuenta Vista',1)
 GO
 INSERT INTO CuentasBancaria VALUES (1048,40,'CuentaRut','Cuenta Rut del Banco Estado', 1, 0)
+GO
+INSERT INTO TiposDeTransaccion VALUES ('00100','inactivo',0)
+INSERT INTO TiposDeTransaccion VALUES ('RedCompra','Compra RedCompra', 1)
+INSERT INTO TiposDeTransaccion VALUES ('PagoLinea','Pago en línea', 1)
 
 SELECT * FROM Bancos
 SELECT * FROM TiposDeCuenta
