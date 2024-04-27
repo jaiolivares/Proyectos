345bbff8d3f0692cf1ca9c71aaa045f8653ec977
Javier Olivares Zavala <javier.olivares.zavala@gmail.com>
2024-04-26 23:11:37 -0400

    selenium

---
+++ 03_BoletasDownload/Sln-BoletasDownload/BoletasDownload/Paginas/Entel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoletasDownload.Paginas
+{
+    public class Entel
+    {
+        public void EjecutarEntel()
+        {
+        }
+    }
+}
--- 03_BoletasDownload/Sln-BoletasDownload/BoletasDownload/Program.cs
+++ 03_BoletasDownload/Sln-BoletasDownload/BoletasDownload/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello, World!");
+
+using BoletasDownload.Paginas;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver();
+
+Entel entel = new();
+
+entel.EjecutarEntel();
